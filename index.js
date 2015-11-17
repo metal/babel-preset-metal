3eceb0ff8e7e27fe5c0754fa6852c8fb79c54c34
Maira Bello <maira.araujo@liferay.com>
2015-11-17 16:31:36 -0300

    Adds plugins to preset

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 'use strict';
 
 module.exports = {
+  plugins: [
+    require('babel-plugin-metal-register-components'),
+    require('babel-plugin-import-alias-bower')
+  ]
 };
--- test/test.js
+++ test/test.js
@@ -3,26 +3,25 @@
 var assert = require('assert');
 var babel = require('babel-core');
 var bowerDirectory = require('bower-directory');
-var plugin = require('../index');
+var preset = require('../index');
 var sinon = require('sinon');
 
 module.exports = {
-  setUp: function(done) {
-    sinon.stub(bowerDirectory, 'sync').returns('/path/to/bower');
-    done();
-  },
-
-  tearDown: function(done) {
-    bowerDirectory.sync.restore();
-    done();
-  },
-
   testBowerImport: function(test) {
+    sinon.stub(bowerDirectory, 'sync').returns('/path/to/bower');
     var code = 'import foo from "bower:bar/src/foo";';
-    var result = babel.transform(code, {plugins: [plugin]});
+    var result = babel.transform(code, {presets: [preset]});
 
     var expected = 'import foo from "/path/to/bower/bar/src/foo";';
     assert.strictEqual(expected, result.code);
+    bowerDirectory.sync.restore();
+    test.done();
+  },
+
+  testComponentRegistration: function(test) {
+    var code = 'class Foo extends Bar {}\nexport default Foo;';
+    var result = babel.transform(code, {presets: [preset]});
+    assert.notStrictEqual(-1, result.code.indexOf('Foo.prototype.registerMetalComponent'));
     test.done();
   }
 };
