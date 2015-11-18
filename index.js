51f5b2fc0cb9677eb444e249e1ad96fc0746d842
Maira Bello <maira.araujo@liferay.com>
2015-11-18 15:56:58 -0300

    Adds es2015 preset

--- index.js
+++ index.js
@@ -4,5 +4,8 @@ module.exports = {
   plugins: [
     require('babel-plugin-metal-register-components'),
     require('babel-plugin-import-alias-bower')
+  ],
+  presets: [
+    require('babel-preset-es2015')
   ]
 };
--- test/test.js
+++ test/test.js
@@ -12,8 +12,7 @@ module.exports = {
     var code = 'import foo from "bower:bar/src/foo";';
     var result = babel.transform(code, {presets: [preset]});
 
-    var expected = 'import foo from "/path/to/bower/bar/src/foo";';
-    assert.strictEqual(expected, result.code);
+    assert.notStrictEqual(-1, result.code.indexOf('/path/to/bower/bar/src/foo'));
     bowerDirectory.sync.restore();
     test.done();
   },
@@ -22,6 +21,13 @@ module.exports = {
     var code = 'class Foo extends Bar {}\nexport default Foo;';
     var result = babel.transform(code, {presets: [preset]});
     assert.notStrictEqual(-1, result.code.indexOf('Foo.prototype.registerMetalComponent'));
+    test.done();
+  },
+
+  testEs2015: function(test) {
+    var code = 'class Foo extends Bar {}\nexport default Foo;';
+    var result = babel.transform(code, {presets: [preset]});
+    assert.strictEqual(-1, result.code.indexOf('class Foo'));
     test.done();
   }
 };
