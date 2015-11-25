2d4d081da200f255c066a6b97c26d6633d3be892
Maira Bello <maira.araujo@liferay.com>
2015-11-25 14:04:58 -0300

    Adds comment explaining why we're using loose mode for the classes plugin

--- index.js
+++ index.js
@@ -30,6 +30,10 @@ module.exports = {
   resolveModuleSource: renameAlias,
   plugins: [
     require('babel-plugin-metal-register-components'),
+
+    // Temporary hack that fixes constructor problems for IE <= 10.
+    // See https://phabricator.babeljs.io/T3041 for more details.
+    // TODO: Remove this was the issue is fixed.
     [require('babel-plugin-transform-es2015-classes'), {loose: true}]
   ],
   presets: [
