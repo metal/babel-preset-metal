e1c46e2caf2148cc08a9e22249a886733a08ec5e
Maira Bello <maira.araujo@liferay.com>
2016-09-12 13:07:35 -0300

    Stops using loose mode
    Compatibility with IE <= 10 has been fixed in babel 6.14.0, without the need to use loose mode.

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 'use strict';
 
 module.exports = {
-  plugins: [
-    // Temporary hack that fixes constructor problems for IE <= 10.
-    // See https://phabricator.babeljs.io/T3041 for more details.
-    // TODO: Remove this was the issue is fixed.
-    [require('babel-plugin-transform-es2015-classes'), {loose: true}]
-  ],
   presets: [
     require('babel-preset-es2015')
   ]
